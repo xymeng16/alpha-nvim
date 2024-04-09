744590cd4415bf40f4e8086036c8e8c63c55937e
Xiangyi Meng <xymeng16@gmail.com>
2024-04-09 15:27:02 +0800

    Update startify.lua to center

--- lua/alpha/themes/startify.lua
+++ lua/alpha/themes/startify.lua
@@ -29,11 +29,11 @@ local function button(sc, txt, keybind, keybind_opts)
     local sc_ = sc:gsub("%s", ""):gsub(leader, "<leader>")
 
     local opts = {
-        position = "left",
+        position = "center",
         shortcut = "[" .. sc .. "] ",
         cursor = 1,
         -- width = 50,
-        align_shortcut = "left",
+        align_shortcut = "center",
         hl_shortcut = { { "Operator", 0, 1 }, { "Number", 1, #sc + 1 }, { "Operator", #sc + 1, #sc + 2 } },
         shrink_margin = false,
     }
